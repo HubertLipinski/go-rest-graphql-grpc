804e66d7b4138edb05c88d3d2887e748104925c8
Hubert Lipiński <hubertlipinskipl@gmail.com>
2025-08-02 14:09:49 +0200

    init graphql

--- backend/api/graphql/main.go
+++ backend/api/graphql/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/HubertLipinski/go-rest-graphql-grpc/internal/config"
+	"github.com/HubertLipinski/go-rest-graphql-grpc/internal/database"
+	"github.com/HubertLipinski/go-rest-graphql-grpc/internal/graphql/generated"
+	gql "github.com/HubertLipinski/go-rest-graphql-grpc/internal/graphql/resolver"
+)
 
 func main() {
-	fmt.Println("Hello from GraphQL")
+	log.Print("Starting GraphQL")
+
+	config.LoadEnv()
+
+	connection, err := database.InitDBConnection()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer connection.Close()
+
+	executableSchema := generated.NewExecutableSchema(generated.Config{
+		Resolvers: &gql.Resolver{DB: connection},
+	})
+
+	srv := handler.New(executableSchema)
+
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.Websocket{
+		Upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		},
+	})
+
+	srv.Use(extension.Introspection{})
+
+	http.Handle("/", playground.Handler("GraphQL", "/query"))
+	http.Handle("/query", srv)
+
+	log.Println("GraphQL server listening on http://localhost:8082")
+	log.Fatal(http.ListenAndServe(":8082", nil))
 }
--- backend/go.mod
+++ backend/go.mod
@@ -3,18 +3,26 @@ module github.com/HubertLipinski/go-rest-graphql-grpc
 go 1.24
 
 require (
+	github.com/99designs/gqlgen v0.17.78
+	github.com/gorilla/websocket v1.5.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/prometheus/client_golang v1.22.0
+	github.com/vektah/gqlparser/v2 v2.5.30
 )
 
 require (
+	github.com/agnivade/levenshtein v1.2.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.62.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
-	golang.org/x/sys v0.30.0 // indirect
-	google.golang.org/protobuf v1.36.5 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	golang.org/x/sys v0.34.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
